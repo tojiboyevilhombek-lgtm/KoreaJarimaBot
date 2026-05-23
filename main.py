308f3952cc1e73c47155f2a507835d260ccd9dd4
tojiboyevilhombek-lgtm <tojiboyevilhombek@gmail.com>
2026-05-24 02:41:37 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
-import logging
 import os
 from aiogram import Bot, Dispatcher, executor, types
+from aiohttp import web
 
 API_TOKEN = '7849691664:AAHscZX15aRK-mK8JRDPDfJk3xjbIJdxcXY'
+# Render beradigan portni olamiz
+PORT = int(os.environ.get('PORT', 8080))
 
-logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
-async def send_welcome(message: types.Message):
-    await message.reply("Bot ishga tushdi va ulanish o'rnatildi!")
+async def start(message: types.Message):
+    await message.answer("Bot Webhook orqali ishga tushdi!")
 
-# Render uchun muhim: PORT sozlami
+async def on_startup(dp):
+    # Telegramga botimiz qayerdaligini aytamiz
+    await bot.set_webhook("https://korea-jarima-bot.onrender.com")
+
 if __name__ == '__main__':
-    # Render'da "Web Service" sifatida ishlash uchun 'skip_updates=True' juda muhim
-    executor.start_polling(dp, skip_updates=True)
+    # Webhook rejimida ishga tushiramiz
+    executor.start_webhook(
+        dispatcher=dp,
+        webhook_path='/',
+        on_startup=on_startup,
+        skip_updates=True,
+        host="0.0.0.0",
+        port=PORT,
+    )
