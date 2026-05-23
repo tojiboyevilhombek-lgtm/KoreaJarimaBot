4075bdbfb1bc045c3f788090a167850982b71ba4
tojiboyevilhombek-lgtm <tojiboyevilhombek@gmail.com>
2026-05-24 02:02:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import logging
+import os
 from aiogram import Bot, Dispatcher, executor, types
 
 API_TOKEN = '7849691664:AAHscZX15aRK-mK8JRDPDfJk3xjbIJdxcXY'
@@ -9,11 +10,9 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-    await message.reply("Assalomu alaykum! Korea Avto Jarima Botiga xush kelibsiz.")
+    await message.reply("Bot ishga tushdi va ulanish o'rnatildi!")
 
-@dp.message_handler()
-async def echo(message: types.Message):
-    await message.answer(f"Siz yuborgan raqam: {message.text}")
-
+# Render uchun muhim: PORT sozlami
 if __name__ == '__main__':
+    # Render'da "Web Service" sifatida ishlash uchun 'skip_updates=True' juda muhim
     executor.start_polling(dp, skip_updates=True)
