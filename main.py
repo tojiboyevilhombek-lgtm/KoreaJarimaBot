0ac6ab5e3e9e6d107961d6f39c615a432a174f66
tojiboyevilhombek-lgtm <tojiboyevilhombek@gmail.com>
2026-05-24 12:01:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import os
 from aiogram import Bot, Dispatcher, executor, types
 
+# Railway'dagi Variable dan tokenni o'qiydi
 API_TOKEN = os.getenv('API_TOKEN')
+
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
-    await message.answer("Bot ishlamoqda!")
+    await message.answer("Assalomu alaykum! Bot ishlamoqda!")
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer("Men sizning xabaringizni oldim!")
 
 if __name__ == '__main__':
-    executor.start_polling(dp)
+    # Webhook emas, Polling ishlatamiz
+    executor.start_polling(dp, skip_updates=True)
